471401e903b4e65ace49e8a8b700eef184618ed6
Pinot. Kim <pinot.kim@kakao.com>
2020-06-09 01:16:56 +0900

    :sparkles: Add function login

--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -1,19 +1,74 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
 function LoginPage() {
+  const inital = {
+    email: '',
+    password: '',
+  }
+  const [loginFrmData, setLoginFrmData] = useState(inital)
+
+  const onChange = useCallback(
+    (e) => {
+      setLoginFrmData({
+        ...loginFrmData,
+        [e.target.type]: e.target.value,
+      })
+    },
+    [loginFrmData],
+  )
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault()
+      setLoginFrmData(inital)
+    },
+    [inital],
+  )
+
   return (
     <div>
-      Login Page Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-      accusamus accusantium consequuntur culpa cupiditate dignissimos distinctio
-      dolor dolores ducimus error est eum expedita fuga in iure modi nemo nisi
-      numquam obcaecati praesentium quae quas qui quia quidem quo, quod quos
-      repellendus rerum suscipit tempora totam ullam velit veniam veritatis
-      voluptas voluptates voluptatibus! Accusantium dolores laudantium quasi?
-      Debitis esse nostrum recusandae sed sit ullam voluptate voluptates.
-      Consectetur dolorem libero perspiciatis quas quis. Illo molestiae natus
-      nulla soluta temporibus? Aliquid doloremque maiores minima nihil. Adipisci
-      animi asperiores aspernatur itaque libero neque odio porro praesentium
-      quam qui repellat reprehenderit, velit voluptas, voluptatem voluptates.
+      <div>
+        <img src="https://placekitten.com/500/500" alt="chat-bot logo" />
+      </div>
+
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={loginFrmData.email}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={loginFrmData.password}
+          onChange={onChange}
+        />
+
+        <label htmlFor="rember-me">Remeber me</label>
+        <input type="checkbox" id="rember-me" name="rember-me" />
+
+        <button type="submit">Login</button>
+      </form>
+
+      <div>
+        <span>Or login with</span>
+
+        <div>
+          <img src="https://placekitten.com/50/50" alt="naver logo" />
+          <span>login with naver</span>
+        </div>
+
+        <div>
+          <img src="https://placekitten.com/50/50" alt="google logo" />
+          <span>login with google</span>
+        </div>
+
+        <div>
+          <img src="https://placekitten.com/50/50" alt="kakao logo" />
+          <span>login with kakao</span>
+        </div>
+      </div>
     </div>
   )
 }
