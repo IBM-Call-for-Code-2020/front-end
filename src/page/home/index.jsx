01a66d33f929bcac756c4301b241db68af19bc9c
Pinot. Kim <pinot.kim@kakao.com>
2020-06-08 22:58:20 +0900

    :sparkles: Add default routing and loading component

---
+++ src/page/home/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      Home Page Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+      accusamus accusantium consequuntur culpa cupiditate dignissimos distinctio
+      dolor dolores ducimus error est eum expedita fuga in iure modi nemo nisi
+      numquam obcaecati praesentium quae quas qui quia quidem quo, quod quos
+      repellendus rerum suscipit tempora totam ullam velit veniam veritatis
+      voluptas voluptates voluptatibus! Accusantium dolores laudantium quasi?
+      Debitis esse nostrum recusandae sed sit ullam voluptate voluptates.
+      Consectetur dolorem libero perspiciatis quas quis. Illo molestiae natus
+      nulla soluta temporibus? Aliquid doloremque maiores minima nihil. Adipisci
+      animi asperiores aspernatur itaque libero neque odio porro praesentium
+      quam qui repellat reprehenderit, velit voluptas, voluptatem voluptates.
+    </div>
+  )
+}
+
+export default HomePage
