89bc59ab2c988b2a89b469d85b3cd5449391e7be
Pinot. Kim <pinot.kim@kakao.com>
2020-06-09 04:16:58 +0900

    :lipstick: Add style at Login container

---
+++ src/module/login/index.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const LoginFormModule = styled.form`
+  padding: 0 20px;
+`
+
+const LoginFormInputContainerModule = styled.div`
+  margin-bottom: 40px;
+`
+
+export {
+  LoginFormModule as Form,
+  LoginFormInputContainerModule as InputContainer,
+}
