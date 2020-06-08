a4c8ce3999335c700db772a6f89e335bee4ec38e
Pinot. Kim <pinot.kim@kakao.com>
2020-06-09 00:07:47 +0900

    :sparkles: Add route for debugging loading page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { Login, Home, Chat } from './page'
+import Loading from './page/loading'
 import './App.css'
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/chat" component={Chat} />
+
+        <Route path="/debug/loading" component={Loading} />
       </Switch>
     </>
   )
