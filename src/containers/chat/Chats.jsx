16b57d6b2a30c8e48cd622650cc263462d95721b
Pinot. Kim <pinot.kim@kakao.com>
2020-06-10 22:46:43 +0900

    :sparkles: Change data to state

--- src/containers/chat/Chats.jsx
+++ src/containers/chat/Chats.jsx
@@ -1,48 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Bot from 'components/chat/bubble/Bot'
 import User from 'components/chat/bubble/User'
 
-const data = [
-  {
-    chat: 'bot',
-    content: [
-      {
-        content:
-          'Hi there! I am Carbon Kim, here to help with your carbon tax support.',
-      },
-      {
-        content: 'What can I help you with today?',
-      },
-    ],
-  },
-  {
-    chat: 'user',
-    content: [
-      {
-        content: 'hell yeah fuck',
-      },
-    ],
-  },
-  {
-    chat: 'bot',
-    content: [
-      {
-        content:
-          'Hi there! I am Carbon Kim, here to help with your carbon tax support. What can I help you with today?',
-      },
-      {
-        content:
-          'Hi there! I am Carbon Kim, here to help with your carbon tax support. What can I help you with today?',
-      },
-      {
-        content:
-          'Hi there! I am Carbon Kim, here to help with your carbon tax support. What can I help you with today?',
-      },
-    ],
-  },
-]
-
 function ChatListsContainer(props) {
+  const [data, setData] = useState([
+    {
+      chat: 'bot',
+      content: [
+        {
+          content:
+            'Hi there! I am Carbon Kim, here to help with your carbon tax support.',
+        },
+        {
+          content: 'What can I help you with today?',
+        },
+      ],
+    },
+    {
+      chat: 'user',
+      content: [
+        {
+          content: 'hell yeah fuck',
+        },
+      ],
+    },
+    {
+      chat: 'bot',
+      content: [
+        {
+          content:
+            'Hi there! I am Carbon Kim, here to help with your carbon tax support. What can I help you with today?',
+        },
+        {
+          content:
+            'Hi there! I am Carbon Kim, here to help with your carbon tax support. What can I help you with today?',
+        },
+        {
+          content:
+            'Hi there! I am Carbon Kim, here to help with your carbon tax support. What can I help you with today?',
+        },
+      ],
+    },
+  ])
+
   return (
     <div>
       {data.map((_i, i) => (
